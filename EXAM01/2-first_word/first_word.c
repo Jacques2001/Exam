a59b66a03f2158c1474d084e7e793ee807f9f17f
Lesly Chiu <lesly_qiu@hotmail.fr>
2024-09-19 13:50:28 +0800

    .

--- EXAM01/2-first_word/first_word.c
+++ EXAM01/2-first_word/first_word.c
@@ -7,12 +7,10 @@ int		main(int ac, char **av)
 	i = 0;
 	if (ac == 2)
 	{
-		while (av[1][i] != '\0')
+		while (av[1][i] == ' ' || av[1][i] == '\t')
+			i++;
+		while (av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t')
 		{
-			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				av[1][i] += 32;
-			else if (av[1][i] >= 'a' && av[1][i] <= 'z')
-				av[1][i] -= 32;
 			write(1, &av[1][i], 1);
 			i++;
 		}
